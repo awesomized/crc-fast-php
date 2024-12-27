2870d6cf0c062ebd022f6a42f15bdffd6b5841f0
Don MacAskill <don@smugmug.com>
2024-12-27 09:36:40 -0800

    Remove unused code

--- cli/calculate.php
+++ cli/calculate.php
@@ -12,18 +12,9 @@ if (!isset($argv[1]) || '' === $argv[1]) {
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$headerFile = match (PHP_OS_FAMILY) {
-    'Darwin' => 'crc64nvme-darwin.h',
-    'Windows' => 'crc64nvme-windows.h',
-    default => 'crc64nvme-linux.h',
-};
-
-$ffi = Crc64\Ffi::fromPreloadScope();
-/*
 $ffi = Crc64\Ffi::fromHeaderFile(
     headerFile: Crc64\Ffi::whichHeaderFile(),
 );
-*/
 
 if (is_readable($argv[1])) {
     echo Crc64\Nvme::calculateFile(
